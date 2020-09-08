5e2d3c81e30dc49e7973733817cb1c1236a54ed6
Pontus Fredriksson <pontus.fredriksson1@gmail.com>
2020-09-08 10:55:17 +0200

    Clean up the code

--- ruj3wt/Program.cs
+++ ruj3wt/Program.cs
@@ -4,25 +4,21 @@ namespace ruj3wt
 {
     class Program
     {
-        static void printAmountOfCarsAndAvailableSeats(int persons){
+        static int[] amountOfCarsAndAvailableSeats(int persons){
            int amountOfCars = (persons + 4) / 5;
            int filledCars = persons / 5;
            int unfilledCar = amountOfCars - filledCars;
             int takenSeats = persons % 5;
            int AvaliableSeats = (unfilledCar * 5) - takenSeats;
            
-           System.Console.WriteLine($"for {persons} persons {amountOfCars} cars are required. {AvaliableSeats} available seats."  );
-        }
+           return new int[]{amountOfCars, AvaliableSeats};
+           
         static void Main(string[] args)
         {
-            printAmountOfCarsAndAvailableSeats(0);
-            printAmountOfCarsAndAvailableSeats(1);
-            printAmountOfCarsAndAvailableSeats(14);
-            printAmountOfCarsAndAvailableSeats(15);
-            printAmountOfCarsAndAvailableSeats(16);
+            for(int i = 0; i< 7; i++){
+                int[] answers = amountOfCarsAndAvailableSeats(i);
+                System.Console.WriteLine($"for {i} persons {answers[0]} cars are required. {answers[1]} available seats."  );
+            }            
         }
-
-        
-
     }
 }

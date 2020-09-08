6e9a7cd340d5970a763f42236b8bb8d328e21710
Pontus Fredriksson <pontus.fredriksson1@gmail.com>
2020-09-08 10:45:16 +0200

    Solve the car problem

---
+++ ruj3wt/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ruj3wt
+{
+    class Program
+    {
+        static void printAmountOfCarsAndAvailableSeats(int persons){
+           int amountOfCars = (persons + 4) / 5;
+           int filledCars = persons / 5;
+           int unfilledCar = amountOfCars - filledCars;
+            int takenSeats = persons % 5;
+           int AvaliableSeats = (unfilledCar * 5) - takenSeats;
+           
+           System.Console.WriteLine($"för {persons} personer krävs {amountOfCars} bilar. lediga platser {AvaliableSeats} "  );
+        }
+        static void Main(string[] args)
+        {
+            printAmountOfCarsAndAvailableSeats(0);
+            printAmountOfCarsAndAvailableSeats(1);
+            printAmountOfCarsAndAvailableSeats(14);
+            printAmountOfCarsAndAvailableSeats(15);
+            printAmountOfCarsAndAvailableSeats(16);
+        }
+
+        
+
+    }
+}
